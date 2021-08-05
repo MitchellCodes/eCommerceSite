e50fc0f682e566fa8500addd6e845252432b3adc
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-08-04 19:28:50 -0700

    Created Product model

---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// A salable product
+    /// </summary>
+    public class Product
+    {
+        [Key] // Make Primary Key in database
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The name of the product the consumer sees
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The retail price in US currency
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Category that the product falls under. Ex. Electronics, Furniture, etc.
+        /// </summary>
+        public string Category { get; set; }
+    }
+}

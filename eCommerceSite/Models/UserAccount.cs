bd18a7ad9b5d6a8f5f9e6b17812db0ca61b9808a
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-08-31 12:43:12 -0700

    Created a UserAccount model

---
+++ eCommerceSite/Models/UserAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        // nullable data type
+        public DateTime? DateOfBirth { get; set; }
+    }
+}

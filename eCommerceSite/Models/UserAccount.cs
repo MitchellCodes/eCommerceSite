40f26cac17c431cbc9ceff11ab2768b230a52805
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-08-31 12:56:23 -0700

    Create Register view model

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -20,4 +20,26 @@ namespace eCommerceSite.Models
         // nullable data type
         public DateTime? DateOfBirth { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)] // time is ignored
+        public DateTime? DateOfBirth { get; set; }
+    }
 }

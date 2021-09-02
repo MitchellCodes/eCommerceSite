2b06206cda6351ea550e8e15f9f7a7417b7b2e12
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-09-02 08:17:38 -0700

    Issue#28 shopping cart (#39)
    * Added shopping cart icon to navigation
    
    * Updated shopping cart link in navigation and added Add to Cart button for each product
    
    * Refactored query for getting a single product from database
    
    * Can now add a single product to cart
    
    * Able to store multiple items in cart
    
    * Created basic summary page. Does not work with empty cookie
    
    * Refactored cart controller
    
    * Added message for an empty cart on summary page

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,45 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">The id of the product to add</param>
+        public async Task<IActionResult> Add(int id)
+        {
+            Product p = await ProductDb.GetProductAsync(_context, id);
+
+            CookieHelper.AddProductToCart(_httpContext, p);
+            
+            // redirect back to previous page
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            List<Product> cartProducts = CookieHelper.GetCartProducts(_httpContext);
+            return View(cartProducts);
+        }
+    }
+}
--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -64,16 +64,7 @@ namespace eCommerceSite.Controllers
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
-            // Get product with corresponding id
-            Product p =
-                await (from prod in _context.Products
-                       where prod.ProductId == id
-                       select prod).SingleAsync();
-
-            //Product p2 = await _context
-            //                .Products
-            //                .Where(prod => prod.ProductId == id)
-            //                .SingleAsync();
+            Product p = await ProductDb.GetProductAsync(_context, id);
 
             // pass product to view
             return View(p);
@@ -96,9 +87,7 @@ namespace eCommerceSite.Controllers
         [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
-            Product p = await (from prod in _context.Products
-                         where prod.ProductId == id
-                         select prod).SingleAsync();
+            Product p = await ProductDb.GetProductAsync(_context, id);
 
             return View(p);
         }
@@ -107,9 +96,7 @@ namespace eCommerceSite.Controllers
         [ActionName("Delete")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            Product p = await (from prod in _context.Products
-                         where prod.ProductId == id
-                         select prod).SingleAsync();
+            Product p = await ProductDb.GetProductAsync(_context, id);
 
             _context.Entry(p).State = EntityState.Deleted;
 
